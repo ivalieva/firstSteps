c66712b2123ede5627726ee683c58c22a74c3d10
Irina Valieva <77702578+ivalieva@users.noreply.github.com>
2022-08-29 19:46:25 +0300

    Add files via upload

--- Ian-Solomein-lessons.playground/Contents.swift
+++ Ian-Solomein-lessons.playground/Contents.swift
@@ -22,27 +22,6 @@ var b = 10
 // prohibited: int + double. var up = a + b
 var up2 = Int(a) + b
 // print
-print("""
-Int.min
-//format of the OS = 64 bit
-Int.max
-//has format of the OS
-//UInt = unsigned >=0
-Int64.min
-Int64.max
-UInt64.min
-UInt64.max
-UInt.min
-UInt.max
-Int8.min
-Int8.max
-UInt8.min
-UInt8.max
-Int32.min
-Int32.max
-UInt32.min
-UInt32.max
-""")
 print(color)
 print("Red color value = \(redColor)")
 // Float and Double
@@ -70,41 +49,3 @@ if areYouHappy {
 } else {
     print("Bad")
 }
-// 3. Basic operators
-/*
- Common
- Унарные: prefix: -a, !b (not b) postfix: c!
- Бинарные: for two smth : 1 + 2 - infix
- Тернарные - к трем величинам:  a ? b : c .
- It means if a is true, then make b, if a is false then make c.
- operands: numbers which operators are operating with
- */
- // Particular
- // Operator = (assign)
- let f = 12
- var d = 5
- d = f // now b equals 12
- // Arithmetical operators +, -, *, /, % (остаток от деления)
- var x = -12
- let y = 5
- x + y
- x - y
- x * y
- x / y
- let h = "Hello"
- let g = "swift"
- // contotination for strings using +
- h + g
-x % y
-// 12 / (5 * 2) + 2
-// x % y = x % -y
-let r = 5
-x % r
-// 12 / (5 * -2) + -2
-let w = 12
-let v = -5
-w % v
-//Составные операторы присваивания += и -=
-var o = 1
-o += 2
-// o = o + 2
