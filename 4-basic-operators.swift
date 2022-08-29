df40bb4d57b772a76de0210619a0974927221a0e
Irina Valieva <77702578+ivalieva@users.noreply.github.com>
2022-08-29 19:42:41 +0300

    Add files via upload

--- 4-basic-operators.swift
+++ 4-basic-operators.swift
@@ -1,10 +1,41 @@
-/* Basic Operators - already in prev. files
- unary, binary, or ternary
-Assignment Operator =
-Arithmetic Operators +, -, *, /
-Remainder Operator %
+/* Basic Operators - from Ian Solomein course
+Common
+ Унарные: prefix: -a, !b (not b) postfix: c!
+ Бинарные: for two smth : 1 + 2 - infix
+ Тернарные - к трем величинам:  a ? b : c .
+ It means if a is true, then make b, if a is false then make c.
+ operands: numbers which operators are operating with
+ */
+ // Operator = (assign)
+ let f = 12
+ var d = 5
+ d = f // now b equals 12
+ // Arithmetical operators +, -, *, /, % (остаток от деления)
+ var x = -12
+ let y = 5
+ x + y
+ x - y
+ x * y
+ x / y
+ let h = "Hello"
+ let g = "swift"
+ // contotination for strings using +
+ h + g
+x % y
+// 12 / (5 * 2) + 2
+// x % y = x % -y
+let r = 5
+x % r
+// 12 / (5 * -2) + -2
+let w = 12
+let v = -5
+w % v
+//Составные операторы присваивания += и -=
+var o = 1
+o += 2
+// o = o + 2
+/*
 Unary Minus Operator
-Compound Assignment Operators +=, -=
 Comparison Operators ==, !=, >, <, >=, <=
 if else
 Logical Operators
